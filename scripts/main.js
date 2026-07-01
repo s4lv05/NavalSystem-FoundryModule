392e4df4d3d27ffd9fc2ddf0df6979902b2debfe
s4lv05 <barsalv05@gmail.com>
2026-07-01 13:23:04 +0200

    Try to test button type naval

--- scripts/main.js
+++ scripts/main.js
@@ -9,3 +9,41 @@ Hooks.once("ready", () => {
 
     ui.notification.info("Naval System caricato!");
 });
+
+Hooks.on("preCreateActor", (actor, data, options, userId) => {
+  if (data.type === "nave") return;
+});
+
+Hooks.on("renderActorDirectory", (app, html, data) => {
+  const button = $(`
+    <button class="create-nave">
+      + Nave
+    </button>
+  `);
+
+  html.find(".directory-header").append(button);
+
+  button.click(async () => {
+    await Actor.create({
+      name: "Nuova Nave",
+      type: "character", // base
+      flags: {
+        naval: {
+          isShip: true
+        }
+      }
+    });
+  });
+});
+
+Actor.prototype.isShip = function () {
+  return this.getFlag("naval", "isShip") === true;
+};
+
+Hooks.once("init", () => {
+  Actors.unregisterSheet("core", ActorSheet);
+  Actors.registerSheet("naval", NavalShipSheet, {
+    types: ["character"],
+    makeDefault: false
+  });
+});
