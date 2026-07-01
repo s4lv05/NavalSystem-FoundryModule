4e95fea286b1189050f513265d1b67d13b902172
s4lv05 <barsalv05@gmail.com>
2026-07-01 14:57:21 +0200

    main.js edited

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+Hooks.on("init", function() {
+    console.log("⚓ Naval System Module | init ok 🟢");
+});
+
+Hooks.on("ready", function() {
+    console.log("⚓ Naval System Module | ready ok 🟢");
+});
