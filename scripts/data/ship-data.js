b4fe258d6eedcb017c696758b0e175f843baad7f
s4lv05 <barsalv05@gmail.com>
2026-07-09 23:16:00 +0200

    fix ship-data, lang.it

--- scripts/data/ship-data.js
+++ scripts/data/ship-data.js
@@ -1,6 +1,6 @@
 export class ShipData extends foundry.abstract.TypeDataModel {
     static defineSchema() {
-        const field = foundry.data.fields;
+        const fields = foundry.data.fields;
         return {
             classe: new fields.StringField({ required: true, initial: "Goletta" }),
             scafo: new fields.SchemaField({
